425807c620c41586376e91971841d83c2b195b25
Lenny4 <alexandre.beaujour@hotmail.fr>
2023-03-18 17:21:38 -0400

    use hook widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:deepfacelab_client/screens/dashboard/dashboard_screen.dart';
 import 'package:deepfacelab_client/screens/settings/settings_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:side_navigation/side_navigation.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -16,37 +17,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
-      home: const MainView(),
+      home: MainView(),
     );
   }
 }
 
-class MainView extends StatefulWidget {
-  const MainView({Key? key}) : super(key: key);
-
-  @override
-  _MainViewState createState() => _MainViewState();
-}
-
-class _MainViewState extends State<MainView> {
-  /// Views to display
-  List<Widget> views = const [
-    DashboardScreen(),
-    SettingsScreen(),
-  ];
-
-  /// The currently selected index of the bar
-  int selectedIndex = 0;
+class MainView extends HookWidget {
+  MainView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var views = useState<List<Widget>>([
+      DashboardScreen(),
+      SettingsScreen(),
+    ]);
+
+    var selectedIndex = useState<int>(0);
+
     return Scaffold(
       // The row is needed to display the current view
       body: Row(
         children: [
           /// Pretty similar to the BottomNavigationBar!
           SideNavigationBar(
-            selectedIndex: selectedIndex,
+            selectedIndex: selectedIndex.value,
             items: const [
               SideNavigationBarItem(
                 icon: Icons.dashboard,
@@ -58,14 +52,13 @@ class _MainViewState extends State<MainView> {
               ),
             ],
             onTap: (index) {
-              setState(() {
-                selectedIndex = index;
-              });
+              selectedIndex.value = index;
             },
           ),
+
           /// Make it take the rest of the available width
           Expanded(
-            child: views.elementAt(selectedIndex),
+            child: views.value.elementAt(selectedIndex.value),
           )
         ],
       ),
