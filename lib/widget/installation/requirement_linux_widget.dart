cbbbd3d55ff150c7e2c33291a7e4509e8c2a2364
Lenny4 <alexandre.beaujour@hotmail.fr>
2023-04-02 17:29:17 -0400

    UI requirement

--- lib/widget/installation/requirement_linux_widget.dart
+++ lib/widget/installation/requirement_linux_widget.dart
@@ -124,16 +124,19 @@ fi
       updateRequirements();
     }, [condaInstallFolder.value]);
 
-    return (requirements.value != null)
-        ? Container(
-            margin: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MarkdownBody(
-                  selectable: true,
-                  data: """# Requirements
-
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const MarkdownBody(
+            selectable: true,
+            data: """# Requirements""",
+          ),
+          if (requirements.value != null) ...[
+            MarkdownBody(
+              selectable: true,
+              data: """
 ${requirements.value!['hasWget'] == true ? "✅ `wget`" : "❌ `wget` was not found by running command `which wget`, just run `sudo apt install wget -y` to install it"}
 
 ${requirements.value!['hasBash'] == true ? "✅ `bash`" : "❌ `bash` was not found by running command `which bash`, just run `sudo apt install bash -y` to install it"}
@@ -142,80 +145,79 @@ ${requirements.value!['hasGit'] == true ? "✅ `git`" : "❌ `git` was not found
 
 ${requirements.value!['hasFfmpeg'] == true ? "✅ `ffmpeg`" : "❌ `ffmpeg` was not found by running command `which ffmpeg`, just run `sudo apt install ffmpeg -y` to install it"}
                   """,
-                ),
-                requirements.value!['hasConda'] == true
-                    ? Container(
-                        margin: const EdgeInsets.only(top: 10.0),
-                        child: RichText(
-                          text: TextSpan(
-                            children: [
-                              const TextSpan(
-                                text: "✅ ",
-                              ),
-                              TextSpan(
-                                  text: "conda",
-                                  style: TextStyle(
-                                    background: Paint()..color = Colors.white10,
-                                  )),
-                            ],
+            ),
+            requirements.value!['hasConda'] == true
+                ? Container(
+                    margin: const EdgeInsets.only(top: 10.0),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: "✅ ",
                           ),
-                        ),
-                      )
-                    : Container(
-                        margin: const EdgeInsets.only(top: 10.0),
-                        child: RichText(
-                          text: TextSpan(
-                            children: [
-                              const TextSpan(
-                                text: "❌ ",
-                              ),
-                              TextSpan(
-                                  text: "conda",
-                                  style: TextStyle(
-                                    background: Paint()..color = Colors.white10,
-                                  )),
-                              const TextSpan(
-                                text: " was not found by running command ",
-                              ),
-                              TextSpan(
-                                  text: "which conda",
-                                  style: TextStyle(
-                                    background: Paint()..color = Colors.white10,
-                                  )),
-                              const TextSpan(
-                                text: ", to install ",
-                              ),
-                              TextSpan(
-                                  text: "conda",
-                                  style: TextStyle(
-                                    background: Paint()..color = Colors.white10,
-                                  )),
-                              TextSpan(
-                                text: ' follow the tutorial',
-                                style: const TextStyle(color: Colors.blue),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    launchUrl(
-                                        Uri.parse(
-                                            'https://docs.conda.io/projects/conda/en/latest/user-guide/install/'),
-                                        mode: LaunchMode.platformDefault);
-                                  },
-                              ),
-                            ],
-                          ),
-                        ),
+                          TextSpan(
+                              text: "conda",
+                              style: TextStyle(
+                                background: Paint()..color = Colors.white10,
+                              )),
+                        ],
                       ),
-                StoreConnector<AppState, HasRequirementsViewModel>(
-                    builder:
-                        (BuildContext context, HasRequirementsViewModel vm) {
-                      return !vm.hasRequirements
-                          ? Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.only(top: 50.0),
-                                  child: const MarkdownBody(
-                                      selectable: true, data: """
+                    ),
+                  )
+                : Container(
+                    margin: const EdgeInsets.only(top: 10.0),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: "❌ ",
+                          ),
+                          TextSpan(
+                              text: "conda",
+                              style: TextStyle(
+                                background: Paint()..color = Colors.white10,
+                              )),
+                          const TextSpan(
+                            text: " was not found by running command ",
+                          ),
+                          TextSpan(
+                              text: "which conda",
+                              style: TextStyle(
+                                background: Paint()..color = Colors.white10,
+                              )),
+                          const TextSpan(
+                            text: ", to install ",
+                          ),
+                          TextSpan(
+                              text: "conda",
+                              style: TextStyle(
+                                background: Paint()..color = Colors.white10,
+                              )),
+                          TextSpan(
+                            text: ' follow the tutorial',
+                            style: const TextStyle(color: Colors.blue),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                launchUrl(
+                                    Uri.parse(
+                                        'https://docs.conda.io/projects/conda/en/latest/user-guide/install/'),
+                                    mode: LaunchMode.platformDefault);
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+            StoreConnector<AppState, HasRequirementsViewModel>(
+                builder: (BuildContext context, HasRequirementsViewModel vm) {
+                  return !vm.hasRequirements
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(top: 50.0),
+                              child:
+                                  const MarkdownBody(selectable: true, data: """
 ## Install by yourself
                     
 You need to install the missing packages, and add it to your `PATH` if necessary
@@ -223,82 +225,77 @@ You need to install the missing packages, and add it to your `PATH` if necessary
 - You can do it yourself and then click on `Recheck my requirements`.
 - Or you can click on `Install for me` and DeepFaceLabClient will try to install all missing packages for you.
 """),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(
-                                      top: 30.0, bottom: 30.0),
-                                  child: ElevatedButton.icon(
-                                    onPressed: loading.value
-                                        ? null
-                                        : onUpdateRequirements,
-                                    icon: loading.value
-                                        ? const CircularProgressIndicator(
-                                            color: Colors.white,
-                                          )
-                                        : const SizedBox.shrink(),
-                                    label:
-                                        const Text('Recheck my requirements'),
-                                  ),
-                                ),
-                                DividerWithTextWidget(text: "OR"),
-                                Container(
-                                    margin: const EdgeInsets.only(
-                                        top: 30.0, bottom: 30.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        const MarkdownBody(
-                                            selectable: true, data: """
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(
+                                  top: 30.0, bottom: 30.0),
+                              child: ElevatedButton.icon(
+                                onPressed:
+                                    loading.value ? null : onUpdateRequirements,
+                                icon: loading.value
+                                    ? const CircularProgressIndicator(
+                                        color: Colors.white,
+                                      )
+                                    : const SizedBox.shrink(),
+                                label: const Text('Recheck my requirements'),
+                              ),
+                            ),
+                            DividerWithTextWidget(text: "OR"),
+                            Container(
+                                margin: const EdgeInsets.only(
+                                    top: 30.0, bottom: 30.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const MarkdownBody(
+                                        selectable: true, data: """
 ## Let DeepFaceLabClient try to install
 """),
-                                        if (requirements.value!['hasConda'] ==
-                                            false) ...[
-                                          Row(
-                                            children: [
-                                              MarkdownBody(
-                                                  selectable: true, data: """
-`conda` will be install in this folder `${condaInstallFolder.value}`
-"""),
-                                              IconButton(
-                                                icon: const Icon(Icons.folder),
-                                                splashRadius: 20,
-                                                onPressed: selectFolder,
-                                              ),
-                                            ],
-                                          ),
+                                    if (requirements.value!['hasConda'] ==
+                                        false) ...[
+                                      Row(
+                                        children: [
                                           MarkdownBody(
                                               selectable: true, data: """
+`conda` will be install in this folder `${condaInstallFolder.value}`
+"""),
+                                          IconButton(
+                                            icon: const Icon(Icons.folder),
+                                            splashRadius: 20,
+                                            onPressed: selectFolder,
+                                          ),
+                                        ],
+                                      ),
+                                      MarkdownBody(selectable: true, data: """
 please click on the folder icon to change the location (you must have write permission to this folder as user `${whoami.value}`)
 
 Note that DeepFaceLabClient will add a path in your `/etc/environment` file to add `conda` in your `PATH`,
 when the installation is done you will need to restart your computer.
 """),
-                                        ],
-                                        Container(
-                                          margin:
-                                              const EdgeInsets.only(top: 10.0),
-                                          child: StartProcessWidget(
-                                            label: "Install for me",
-                                            startProcesses:
-                                                startProcesses.value,
-                                            callback: onInstallationDone,
-                                          ),
-                                        ),
-                                      ],
-                                    )),
-                                Container(
-                                    margin: const EdgeInsets.only(top: 10.0),
-                                    child: OpenIssueWidget()),
-                              ],
-                            )
-                          : const SizedBox.shrink();
-                    },
-                    converter: (store) =>
-                        HasRequirementsViewModel.fromStore(store))
-              ],
-            ),
-          )
-        : const SizedBox.shrink();
+                                    ],
+                                    Container(
+                                      margin: const EdgeInsets.only(top: 10.0),
+                                      child: StartProcessWidget(
+                                        label: "Install for me",
+                                        startProcesses: startProcesses.value,
+                                        callback: onInstallationDone,
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                            Container(
+                                margin: const EdgeInsets.only(top: 10.0),
+                                child: OpenIssueWidget()),
+                          ],
+                        )
+                      : const SizedBox.shrink();
+                },
+                converter: (store) => HasRequirementsViewModel.fromStore(store))
+          ] else ...[
+            const CircularProgressIndicator()
+          ],
+        ],
+      ),
+    );
   }
 }
