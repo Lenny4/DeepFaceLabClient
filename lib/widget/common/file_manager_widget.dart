c0b912b5bb99591d6ff7a9327645e8baab932e2f
Lenny4 <alexandre.beaujour@hotmail.fr>
2023-04-03 13:08:07 -0400

    fix loadFilesFolders

--- lib/widget/common/file_manager_widget.dart
+++ lib/widget/common/file_manager_widget.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:path/path.dart' as p;
 
 class FileManagerWidget extends HookWidget {
   final String initPath;
@@ -12,14 +13,14 @@ class FileManagerWidget extends HookWidget {
   @override
   Widget build(BuildContext context) {
     var folderPath = useState<String>(initPath);
-    var fileSystemEntities = useState<List<Map<String, dynamic>>>([]);
+    var fileSystemEntities = useState<List<Map<String, dynamic>>?>(null);
 
     loadFilesFolders() async {
+      fileSystemEntities.value = null;
       fileSystemEntities.value =
           await (Directory(folderPath.value).list()).map((fileSystemEntity) {
         // https://stackoverflow.com/questions/75915594/pathinfo-method-equivalent-for-dart-language#answer-75915804
-        String filename = fileSystemEntity.path
-            .substring(fileSystemEntity.path.lastIndexOf('/') + 1);
+        String filename = p.basename(fileSystemEntity.path);
         return {
           'filename': filename,
           'directory': fileSystemEntity is Directory,
@@ -38,49 +39,56 @@ class FileManagerWidget extends HookWidget {
       loadFilesFolders();
     }, [folderPath.value]);
 
-    return fileSystemEntities.value.isNotEmpty
-        ? Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("header"),
-                Expanded(
-                  child: GridView.builder(
-                      // https://stackoverflow.com/questions/53612200/flutter-how-to-give-height-to-the-childrens-of-gridview-builder
-                      // https://www.youtube.com/watch?v=0blNt4XIi0g
-                      gridDelegate:
-                          const SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 100,
-                      ),
-                      itemCount: fileSystemEntities.value.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Tooltip(
-                          message: fileSystemEntities.value[index]['filename'],
-                          child: Card(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                fileSystemEntities.value[index]['directory']
-                                    ? const Icon(Icons.folder, size: 50)
-                                    : fileSystemEntities.value[index]['image']
-                                        ? Image.asset(
-                                            height: 70,
-                                            ("${folderPath.value}/${fileSystemEntities.value[index]['filename']}"))
-                                        : const Icon(Icons.file_open, size: 50),
-                                Text(
-                                    fileSystemEntities.value[index]['filename'],
-                                    maxLines: 1),
-                              ],
-                            ),
+    return fileSystemEntities.value == null
+        ? const Center(child: CircularProgressIndicator())
+        : fileSystemEntities.value!.isNotEmpty
+            ? Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("header"),
+                    Expanded(
+                      child: GridView.builder(
+                          // https://stackoverflow.com/questions/53612200/flutter-how-to-give-height-to-the-childrens-of-gridview-builder
+                          // https://www.youtube.com/watch?v=0blNt4XIi0g
+                          gridDelegate:
+                              const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 100,
                           ),
-                        );
-                      }),
+                          itemCount: fileSystemEntities.value!.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Tooltip(
+                              message: fileSystemEntities.value![index]
+                                  ['filename'],
+                              child: Card(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    fileSystemEntities.value![index]
+                                            ['directory']
+                                        ? const Icon(Icons.folder, size: 50)
+                                        : fileSystemEntities.value![index]
+                                                ['image']
+                                            ? Image.asset(
+                                                height: 70,
+                                                ("${folderPath.value}/${fileSystemEntities.value![index]['filename']}"))
+                                            : const Icon(Icons.file_open,
+                                                size: 50),
+                                    Text(
+                                        fileSystemEntities.value![index]
+                                            ['filename'],
+                                        maxLines: 1),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                    const Text("footer 1"),
+                  ],
                 ),
-                const Text("footer 1"),
-              ],
-            ),
-          )
-        : const MarkdownBody(
-            selectable: true, data: "# Your directory is empty");
+              )
+            : const MarkdownBody(
+                selectable: true, data: "# Your directory is empty");
   }
 }
