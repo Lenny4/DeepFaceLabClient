08cbc3c9f8297f755a0fc13105a708a5ad8829b2
Rajat Mishra <77861069+mst-rajatmishra@users.noreply.github.com>
2024-09-16 16:24:57 +0530

    Update getDevices.py

--- script/python/getDevices.py
+++ script/python/getDevices.py
@@ -1,36 +1,23 @@
-# https://stackoverflow.com/questions/4383571/importing-files-from-different-folder#answer-4383597
 import json
 import sys
+import os
 
-# caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '%deepFaceLabFolder%')
+# Set up the path to include the directory of your modules
+deepFaceLabFolder = os.getenv('DEEPLFACELAB_FOLDER', '/path/to/deepFaceLabFolder')
+sys.path.insert(1, deepFaceLabFolder)
 
 from core.leras.device import Devices
 
+# Initialize the environment
 Devices.initialize_main_env()
+
 all_devices = []
-for device in Devices.getDevices():
-    deviceJson = {}
-    # https://stackoverflow.com/questions/25150955/python-iterating-through-object-attributes#answer-25151000
-    for attr, value in device.__dict__.items():
-        deviceJson[attr] = value
-    all_devices.append(deviceJson)
-# all_devices.append({
-#     'index': 0,
-#     'tf_dev_type': 'tf_dev_type',
-#     'name': 'name',
-#     'total_mem': 3221225472,
-#     'total_mem_gb': 3221225472 / 1024 ** 3,
-#     'free_mem': 3221225472,
-#     'free_mem_gb': 3221225472 / 1024 ** 3,
-# })
-# all_devices.append({
-#     'index': 1,
-#     'tf_dev_type': 'tf_dev_type',
-#     'name': 'name',
-#     'total_mem': 3221225472,
-#     'total_mem_gb': 3221225472 / 1024 ** 3,
-#     'free_mem': 3221225472,
-#     'free_mem_gb': 3221225472 / 1024 ** 3,
-# })
-print(json.dumps(all_devices), end='')
+try:
+    for device in Devices.getDevices():
+        deviceJson = {attr: value for attr, value in device.__dict__.items()}
+        all_devices.append(deviceJson)
+except Exception as e:
+    print(f"An error occurred: {e}", file=sys.stderr)
+
+# Convert device data to JSON format and print it
+print(json.dumps(all_devices, indent=2), end='')
